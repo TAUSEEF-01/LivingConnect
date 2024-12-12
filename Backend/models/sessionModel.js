c9ef7ccccc6d7b94c1bb356a7b8de5a883f003b0
TAUSEEF-01 <thesupremeemperor01@gmail.com>
2024-12-13 05:21:43 +0600

    clean code

--- Backend/models/sessionModel.js
+++ Backend/models/sessionModel.js
@@ -1,17 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const sessionSchema = new mongoose.Schema({
-//     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-//     token: { type: String, required: true },
-//     createdAt: { type: Date, default: Date.now, expires: "2h" } // Automatically delete after 2 hours
-//   });
-  
-
-// const YourSessionModel = mongoose.model("Session", sessionSchema);
-// module.exports = YourSessionModel;
-
-
-
 const mongoose = require("mongoose");
 
 const sessionSchema = new mongoose.Schema({
